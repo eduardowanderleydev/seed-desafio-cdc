feae4219dd820bf45326914550568d380605fd4b
Eduardo Wanderley <eduardo.wanderley@alura.com.br>
2023-01-06 14:09:23 -0300

    Implementing show books endpoint

--- cdc/src/main/java/br/com/deveficiente/cdc/book/BookController.java
+++ cdc/src/main/java/br/com/deveficiente/cdc/book/BookController.java
@@ -5,11 +5,14 @@ import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-// 2 points of cognitive load
+import java.util.List;
+
+// 4 points of cognitive load
 @RestController
 public class BookController {
 
@@ -23,4 +26,9 @@ public class BookController {
         return ResponseEntity.ok().build();
     }
 
+    @GetMapping("/books")
+    public ResponseEntity listBooks() {
+        List<BookView> books = (List<BookView>) entityManager.createQuery("select new br.com.deveficiente.cdc.book.BookView(b.id, b.title) from Book b").getResultList();
+        return ResponseEntity.ok(books);
+    }
 }
---
+++ cdc/src/main/java/br/com/deveficiente/cdc/book/BookView.java
@@ -0,0 +1,3 @@
+package br.com.deveficiente.cdc.book;
+
+public record BookView(Long id, String title) {}
